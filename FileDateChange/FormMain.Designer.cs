13a8792dc9003f4ff20dd4e531509863988a9e1e
Vitaliy <mitradis@gmail.com>
2025-07-04 14:07:22 +0300

    Upload

---
+++ FileDateChange/FormMain.Designer.cs
@@ -0,0 +1,238 @@
+﻿using System.Windows.Forms;
+
+namespace FileDateChange
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ToolTip toolTip1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(336, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Обзор";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(101, 227);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(142, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.Value = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(7, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Изменить дату";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(101, 260);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(152, 260);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(203, 260);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(336, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 25);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Очистить";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(8, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(462, 212);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(450, 229);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(20, 20);
+            this.checkBox1.TabIndex = 10;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.checkBox2.Location = new System.Drawing.Point(250, 244);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Изменен";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.checkBox3.Location = new System.Drawing.Point(250, 224);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 20);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "Создан";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.checkBox4.Location = new System.Drawing.Point(250, 264);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 20);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Открыт";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(442, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(29, 25);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "?";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FormMain
+            // 
+            this.AllowDrop = true;
+            this.ClientSize = new System.Drawing.Size(478, 288);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Date Change";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ FileDateChange/FormMain.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FileDateChange
+{
+    public partial class FormMain : Form
+    {
+        string lastPath = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            dateTimePicker1.Value = DateTime.Now;
+            toolTip1.SetToolTip(checkBox1, "Включая подпапки.");
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            if (lastPath != null)
+            {
+                openFileDialog1.InitialDirectory = lastPath;
+            }
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK && openFileDialog1.FileNames.Length > 0)
+            {
+                lastPath = Path.GetDirectoryName(openFileDialog1.FileNames[0]);
+                listBox1.Items.AddRange(openFileDialog1.FileNames);
+                button2.Enabled = true;
+                Text = "File Date Change " + "(" + listBox1.Items.Count + ")";
+            }
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                dateTimePicker1.Value = new DateTime(dateTimePicker1.Value.Year, dateTimePicker1.Value.Month, dateTimePicker1.Value.Day, Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value), Convert.ToInt32(numericUpDown3.Value));
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    touchFile(listBox1.Items[i].ToString());
+                }
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+            }
+        }
+
+        void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            button2.Enabled = false;
+            Text = "File Date Change";
+        }
+
+        void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                if (File.Exists(listBox1.Items[0].ToString()))
+                {
+                    dateTimePicker1.Value = File.GetLastWriteTime(listBox1.Items[0].ToString());
+                }
+                else if (Directory.Exists(listBox1.Items[0].ToString()))
+                {
+                    dateTimePicker1.Value = Directory.GetLastWriteTime(listBox1.Items[0].ToString());
+                }
+                numericUpDown1.Value = dateTimePicker1.Value.Hour;
+                numericUpDown2.Value = dateTimePicker1.Value.Minute;
+                numericUpDown3.Value = dateTimePicker1.Value.Second;
+            }
+        }
+
+        void touchFile(string file)
+        {
+            try
+            {
+                if (Directory.Exists(file))
+                {
+                    if (checkBox2.Checked)
+                    {
+                        Directory.SetLastWriteTime(file, dateTimePicker1.Value);
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        if (checkBox2.Checked)
+                        {
+                            Thread.Sleep(50);
+                        }
+                        Directory.SetCreationTime(file, dateTimePicker1.Value);
+                    }
+                    if (checkBox4.Checked)
+                    {
+                        if (checkBox2.Checked || checkBox3.Checked)
+                        {
+                            Thread.Sleep(50);
+                        }
+                        Directory.SetLastAccessTime(file, dateTimePicker1.Value);
+                    }
+                }
+                else if (File.Exists(file))
+                {
+                    if (checkBox2.Checked)
+                    {
+                        File.SetLastWriteTime(file, dateTimePicker1.Value);
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        if (checkBox2.Checked)
+                        {
+                            Thread.Sleep(50);
+                        }
+                        File.SetCreationTime(file, dateTimePicker1.Value);
+                    }
+                    if (checkBox4.Checked)
+                    {
+                        if (checkBox2.Checked || checkBox3.Checked)
+                        {
+                            Thread.Sleep(50);
+                        }
+                        File.SetLastAccessTime(file, dateTimePicker1.Value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось изменить в: " + file + Environment.NewLine + ex.Message);
+            }
+        }
+
+        void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string line1 in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                listBox1.Items.Add(line1);
+                if (checkBox1.Checked && Directory.Exists(line1))
+                {
+                    foreach (string line2 in Directory.GetFileSystemEntries(line1, "*", SearchOption.AllDirectories))
+                    {
+                        listBox1.Items.Add(line2);
+                    }
+                }
+            }
+            button2.Enabled = true;
+            Text = "File Date Change " + "(" + listBox1.Items.Count + ")";
+        }
+
+        void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+    }
+}
